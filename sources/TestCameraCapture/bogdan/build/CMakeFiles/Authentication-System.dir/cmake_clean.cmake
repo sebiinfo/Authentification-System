3b3f61382075491f7848f55f377392a062dccac2
Bogdan Budura <bogdanbudura@gmail.com>
2022-12-04 20:41:45 +0100

    A new test directory

---
+++ sources/TestCameraCapture/bogdan/build/CMakeFiles/Authentication-System.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Authentication-System"
+  "Authentication-System.pdb"
+  "CMakeFiles/Authentication-System.dir/test.cpp.o"
+  "CMakeFiles/Authentication-System.dir/test.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Authentication-System.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ sources/TestCameraCapture/bogdan/build/CMakeFiles/Authentication-System.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Authentication-System.
+# This may be replaced when dependencies are built.
