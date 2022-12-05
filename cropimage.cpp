e171744b3c156eda9f0606af4b5ebb382e39b652
rompoggi <ropoggi323@gmail.com>
2022-12-06 00:26:55 +0100

    Update cropimage.cpp and cropimage.hpp
    Fixed issues, now face recognition working properly, started to work on the final function, but issues still.

--- cropimage.cpp
+++ cropimage.cpp
@@ -2,17 +2,15 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include "cropimage.hpp"
 
 using namespace cv;
 using namespace std;
 
 
-void inplaceCrop (Mat image ,int w1,int w2,int h1,int h2){
-    image = image(Range(w1,w2), Range(h1,h2));
-    };
-
-Mat Crop(Mat image ,int w1,int w2,int h1,int h2){
-    return image(Range(w1,w2), Range(h1,h2));
+void cropOne (Mat& image ,int x1,int x2,int y1,int y2){
+    // image(Range(start_row, end_row), Range(start_col, end_col))
+    image = image(Range(y1,y2), Range(x1,x2));
     };
 
 Mat getFrame(){
@@ -26,111 +24,140 @@ Mat getFrame(){
 }
 
 
-void showFrame(Mat frame,string name="Frame"){
-    for(;;){
-        imshow("webcam",frame);
+void showFrame(Mat frame){
+    while(true)
+    {
+        char c = (char)waitKey(10);
 
-        if(waitKey(30)>=0){break;}
+        // Press q to exit from window
+        if( c == 27 || c == 'q' || c == 'Q' )
+            break;
+
+        imshow("Cam",frame);
     }
 }
 
 void runwebcam(){
-    VideoCapture cap(0);
-    if (!cap.isOpened()){
-        return;
-    };
-    for(;;){
-        Mat frame;
-        cap >> frame;
-        imshow("webcam",frame);
+    VideoCapture capture;
+    Mat frame;
 
-        if(waitKey(30)>=0){break;}
-    }
-}
-
-vector<Rect> CoordFace(Mat frame,string path,double scale,Size size)
-{
-
-    CascadeClassifier faceCascade;
-    faceCascade.load(path);
-
-
-    Mat grayscale;
-    cvtColor(frame,grayscale,COLOR_BGR2GRAY);
-    resize(grayscale,grayscale, Size(grayscale.rows / scale, grayscale.cols / scale ) );
-
-
-    vector<Rect> faces;
-    faceCascade.detectMultiScale(grayscale,faces,1.1,3,0,size);
-
-    return faces;
-};
-
-
-void detectface(Mat frame,string path,double scale =1.0,Size size=Size(50,50),Scalar drawColor = Scalar(255,0,0)){
-
-    CascadeClassifier faceCascade;
-    faceCascade.load(path);
-
-    Mat grayscale;
-    cvtColor(frame,grayscale,COLOR_BGR2GRAY);
-//    equalizeHist( grayscale, grayscale );
-
-    resize(grayscale,grayscale, Size(grayscale.rows / scale, grayscale.cols / scale ) );
-
-    vector<Rect> faces;
-    faceCascade.detectMultiScale(grayscale,faces,1.1,3,0,Size(50,50));
-
-
-    for(Rect area : faces)
+    if (capture.isOpened())
     {
-       Point p1 = Point(cvRound(area.x * scale),cvRound(area.y * scale));
-       Point p2 = Point(cvRound((area.x + area.width-1) * scale),cvRound((area.y+area.height -1)* scale));
-       rectangle(frame,p1,p2,drawColor);
-    }
-
-     showFrame(frame);
-}
-
-void runCascadeFace(string path)
-{
-    CascadeClassifier faceCascade;
-    faceCascade.load(path);
-
-    double scale = 1.0;
-    Size size = Size(50,50);
-
-    VideoCapture cap(0);
-    if (!cap.isOpened()){
-        return;
-    };
-
-    Scalar drawColor = Scalar(255,0,0);
-
-    for(;;){
-        Mat frame;
-        cap >> frame;
-
-
-        vector<Rect> faces = CoordFace(frame,path,scale,size);
-
-
-        for(Rect area : faces)
+        while(true)
         {
-           Point p1 = Point(cvRound(area.x * scale),cvRound(area.y * scale));
-           Point p2 = Point(cvRound((area.x + area.width-1) * scale),cvRound((area.y+area.height -1)* scale));
-           rectangle(frame,p1,p2,drawColor);
+            capture >> frame;
+            if(frame.empty())
+                break;
+
+            char c = (char)waitKey(10);
+
+            // Press q to exit from window
+            if( c == 27 || c == 'q' || c == 'Q' )
+                break;
+
+            imshow("Cam",frame);
         }
-
-        imshow("webcam",frame);
-
-        if(waitKey(30)>=0){break;}
     }
+}
+
+void draw(Mat& img, vector<Rect> faces,double scale)
+{
+
+    int c = 50;
+    for (size_t i = 0; i < faces.size(); i++)
+    {
+        Rect r = faces[i];
+        Mat smallImgROI;
+        Scalar color = Scalar(255, 0, 0); // Color for Drawing tool
+        rectangle(img, Point(cvRound(r.x*scale-c), cvRound(r.y*scale-c)),
+                    Point(cvRound((r.x + r.width-1)*scale+c),
+                    cvRound((r.y + r.height-1)*scale+c)), color, 3, 8, 0);
+    }
+}
+
+
+vector<Rect> detect(Mat& img, CascadeClassifier& cascade, double scale){
+    vector<Rect> faces;
+    Mat gray, smallImg;
+
+    cvtColor(img, gray, COLOR_BGR2GRAY); // Convert to Gray Scale
+    double fx = 1 / scale;
+
+    // Resize the Grayscale Image
+    resize(gray, smallImg, Size(), fx, fx, INTER_LINEAR);
+    equalizeHist(smallImg, smallImg);
+
+    // Detect faces of different sizes using cascade classifier
+    cascade.detectMultiScale(smallImg, faces, 1.1,
+                            2, 0|CASCADE_SCALE_IMAGE, Size(30, 30));
+    return faces;
+}
+
+void runCascadeFace(string path){
+    VideoCapture capture;
+    Mat frame, image;
+
+    CascadeClassifier cascade;
+    double scale=5;
+
+    cascade.load(path);
+
+    capture.open(0);
+    if (capture.isOpened())
+    {
+        while(true)
+        {
+            capture >> frame;
+            if(frame.empty())
+                break;
+            Mat frame1 = frame.clone();
+            vector<Rect> faces = detect(frame1,cascade,scale);
+            draw(frame1,faces,scale);
+            char c = (char)waitKey(10);
+
+            // Press q to exit from window
+            if( c == 27 || c == 'q' || c == 'Q' )
+                break;
+
+            imshow("Cam",frame1);
+        }
+    }
+    else {cout<<"Could not Open Camera";}
 
 }
 
-//int main(int argc, char **argv)
-//{
-//  return 0;
-//}
+// DOES NOT WORK YET
+vector<Mat> cropWithRect(Mat frame, vector<Rect> faces)
+{
+    vector<Mat> frameVect;
 
+    for (size_t i = 0; i < faces.size(); i++)
+    {
+        Rect r = faces[i];
+
+//        Mat smallImgROI = frame.clone();
+        cout << 2;
+        cropOne(frame,r.x,r.x+r.width,r.y,r.y+r.height);
+        cout << 3;
+
+        frameVect.push_back(frame.clone());
+//        frame.copyTo(frameList.back());
+        cout << 4;
+
+    };
+    return frameVect;
+}
+
+// SHOULD WORK WHEN cropWithRect FIXED
+vector<Mat> crop(Mat frame,string path)
+{
+    double scale = 1;
+    CascadeClassifier cascade;
+    cascade.load(path);
+
+    vector<Rect> faces = detect(frame,cascade,scale);
+
+    return cropWithRect(frame,faces);
+
+//    return vector<Mat>();
+}
--- cropimage.hpp
+++ cropimage.hpp
@@ -7,30 +7,47 @@
 #include <vector>
 #include <string>
 
+
+//                                "                                       #
+//   mmm    m mm   mmm   mmmm   mmm    mmmmm   mmm    mmmm   mmm          # mm   mmmm   mmmm
+//  #"  "   #"  " #" "#  #" "#    #    # # #  "   #  #" "#  #"  #         #"  #  #" "#  #" "#
+//  #       #     #   #  #   #    #    # # #  m"""#  #   #  #""""         #   #  #   #  #   #
+//  "#mm"   #     "#m#"  ##m#"  mm#mm  # # #  "mm"#  "#m"#  "#mm"    #    #   #  ##m#"  ##m#"
+//                       #                            m  #                       #      #
+//                       "                             ""                        "      "
+
+
 using namespace cv;
 using namespace std;
 
-void inplaceCrop (Mat image, int botLeft, int botRight, int topLeft, int topRight);
+void cropOne (Mat& image, int botLeft, int botRight, int topLeft, int topRight);
 // Crops in place the image at given coordinates (bottom left, bottom right, top left, top right)
-
-void runwebcam();
-// Opens the computer's webcam
 
 Mat getFrame();
 // Get one frame from the webcam
 
-void showFrame(Mat,string);
-// Displays the frame given as input with window title name, "Frame" by default
+void showFrame(Mat image);
+// Displays the frame given as input.
 
-Mat Crop(Mat image, int botLeft, int botRight, int topLeft, int topRight);
-// Returns a cropped version of the image without modifying it at coordinates (bottom left, bottom right, top left, top right)
+void runwebcam();
+// Opens the computer's webcam
 
-vector<Rect> CoordFace(Mat image,string pathname,double scale,Size size);
-// returns a potential position of a face in the image using the haarcascade xml file at path, using in min size Size
+void draw(Mat& img, vector<Rect> faces,double scale);
+//draws the rectangles on the image given as input
 
-void detectface(Mat frame,string path,double scale,Size size=Size(50,50),Scalar drawColor = Scalar(255,0,0));
-// Detects and displays the face on the frame using the haarcascade at path,
-// scaling the image by scale if the face is of size at least size using the color drawColor
+vector<Rect> detect(Mat& img, CascadeClassifier& cascade, double scale);
 
 void runCascadeFace(string path);
 // Continuously detetcs a face using the xml at the given path
+
+// !!!!!!!
+// DOES NOT WORK YET
+// !!!!!!!
+vector<Mat> cropWithRect(Mat frame, vector<Rect> faces);
+// Returns a list of Mat of the faces detected from the frame given
+
+// !!!!!!!
+// DOES NOT WORK YET, DUE TO cropWithRect
+// !!!!!!!
+vector<Mat> crop(Mat frame,string path);
+// Returns a list of Mat cropped around the face recogised by the harrcascade file linked by the path
