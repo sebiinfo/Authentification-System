5432b3178aa93ccefe0d5e12dd4d304fc3c6bf8e
jasminewatissee <jasmine.watissee@gmail.com>
2022-12-05 14:25:28 +0100

    Simplification & optimization of the class

--- password.cpp
+++ password.cpp
@@ -16,49 +16,60 @@ char random_character(){
     return alphanum[rand() % stringLength];
 }
 
-// Class Password
-
-Password::Password() {
-    // Create a random password
-    generate_salt();
-    cout << "Creating a password ..." << endl;
-    password = generate_random();
-    cout << "Your new password is: " << password << endl;
-    encrypt();
-    change_password();
-}
-
-Password::Password(string pwd) {
-    password = pwd;
-    encrypt();
-}
-
-
-Password::~Password() {
-    password.clear();
-    hashed.clear();
-}
-
-bool Password::operator==(Password &other) {
-    other.encrypt();
-    return get_hash() == other.get_hash();
-}
-
-string Password::generate_random() {
-    string s;
-    for(int i=0; i <= 12; i++) {
-        s.push_back(random_character());
+std::vector<std::string> Profile::build_profile(string user, string pwd1, string pwd2){
+    std::vector<string> vect;
+    if (pwd1 == pwd2){
+        user = user;
+        vect.push_back(user);
+        string h = encrypt(new1);
+        hashed = h;
+        vect.push_back(hashed);
     }
-    return s;
+    return vect;
 }
 
-void Password::generate_salt() {
-    for (int i = 0; i < 32; i++) {
-        salt += generate_random();
-        // MAKE SURE SALT IS UNIQUE
+bool Profile::compare_password(string user, string pwd1, string pwd2){
+    //pwd1 is the string entered by the user
+    // pwd2 is the hashed password in the database associated to user
+    string h = encrypt(pwd1);
+    if (pwd1 == pwd2) {
+        return True;
     }
+    return False;
 }
-string Password::hash_it(string CandidatePass){
+
+std::vector<string> Profile::change_password(string user, string old, string new1, string new2){
+    std::vector<string> vect;
+    //Check if user and old password match in database
+    if (new1 == new2){
+        user = user;
+        vect.push_back(user);
+        string h = encrypt(new1);
+        hashed = h;
+        vect.push_back(hashed);
+    }
+    return vect;
+}
+
+string Profile::encrypt(string password){
+    if (salt.empty()){
+            generate_salt();
+            string candidatePass= password+salt;
+            string h;
+            for (int i=0;i<10;i++){
+                h = hash_it(candidatePass);
+            }
+    } else {
+        string candidatePass= password+salt;
+            string h;
+            for (int i=0;i<10;i++){
+                h = hash_it(candidatePass);
+            }
+    }
+    return h;
+}
+
+string Profile::hash_it(string CandidatePass){
         int l1 = CandidatePass.length();
         long long hp = 0;
 
@@ -74,123 +85,173 @@ string Password::hash_it(string CandidatePass){
             string h=to_string(hp);
 
         return h;
-        };
+};
 
-
-void Password::encrypt() {
-    if (! salt.empty()){
-            string candidatePass= password+salt;
-            string h;
-            for (int i=0;i<10;i++){
-                h = hash_it(candidatePass);
-            }
-            hashed=h;
+void Profile::generate_salt(){
+    for (int i = 0; i < 32; i++) {
+        salt += generate_random();
+        // MAKE SURE SALT IS UNIQUE
     }
 }
 
-
-void Password::change_password() {
-    cout << "Would you like to change your password ? (Y/N)" << endl;
-    string answer;
-    cin >> answer;
-    if (answer == "Y"){
-        cout << "Choose a new password: " << endl;
-        string pwd;
-        cin >> pwd;
-        cout << "Confirm your password: " << endl;
-        string pwd2;
-        cin >> pwd2;
-        if (pwd == pwd2) {
-            password = pwd;
-            encrypt();
-        }
-        cout << "Password changed !" << endl;
-    } else {
-        cout << "The password won't be changed" << endl;
+string Profile::generate_random(){
+    string s;
+    for(int i=0; i <= 12; i++) {
+        s.push_back(random_character());
     }
+    return s;
 }
 
-// Class Username
+// Class Password
 
-Username::Username() {
-    string confirm="0";
-    while (confirm!="Y") {
-        string input;
-        cout << "Create your username:";
-        cin >> input;
-        cout << "Your username is: ";
-        cout << input << endl;
-        cout << "Do you confirm? (Y/N)";
-        cin >> confirm;
-        if (confirm == "Y") {
-            user = input;
-            break;
-        } else if (confirm == "N") {
-            continue;
-        }
-        else {
-            while (confirm != "Y" && confirm != "N") {
-                cout << "Do you confirm? (Y/N)";
-            }
-        }
-    }
-}
-bool Username:: operator == (Username &other){
-    return get_user()==other.get_user();
-}
+// Password::Password() {
+//     // Create a random password
+//     generate_salt();
+//     cout << "Creating a password ..." << endl;
+//     password = generate_random();
+//     cout << "Your new password is: " << password << endl;
+//     encrypt();
+//     change_password();
+// }
 
-void Username::change_username(){
-    cout << "Would you like to change your username ? (Y/N)" << endl;
-    string answer;
-    cin >> answer;
-    if (answer == "Y"){
-        cout << "Choose a new username: " << endl;
-        string us;
-        cin >> us;
-        cout << "Confirm your username: " << endl;
-        string us2;
-        cin >> us2;
-        if (us == us2) {
-            user = us;
-        }
-        cout << "Username changed !" << endl;
-    } else {
-        cout << "The username won't be changed" << endl;
-    }
-}
+// Password::Password(string pwd) {
+//     password = pwd;
+//     encrypt();
+// }
 
 
-// Class Profile
+// Password::~Password() {
+//     password.clear();
+//     hashed.clear();
+// }
 
-Profile::Profile(){
-    // Create a profile
-    user=Username();
-    pwd=Password();
-}
+// bool Password::operator==(Password &other) {
+//     other.encrypt();
+//     return get_hash() == other.get_hash();
+// }
+
+// string Password::generate_random() {
+//     string s;
+//     for(int i=0; i <= 12; i++) {
+//         s.push_back(random_character());
+//     }
+//     return s;
+// }
+
+// void Password::generate_salt() {
+//     for (int i = 0; i < 32; i++) {
+//         salt += generate_random();
+//         // MAKE SURE SALT IS UNIQUE
+//     }
+// }
+// string Password::hash_it(string CandidatePass){
+//         int l1 = CandidatePass.length();
+//         long long hp = 0;
+
+//         srand(time(0));
+//         if (random==0) {
+//             random = rand() % (Q - 1) + 1; // generating random value x
+//         }
+//         for(int i=0; i<l1 ;i++) {
+//             hp = (random * hp) % Q; // calculating hash of Password
+//             hp += CandidatePass[i];
+//             hp %= Q;
+//         }
+//             string h=to_string(hp);
+
+//         return h;
+//         };
+
+// void Password::encrypt() {
+//     if (! salt.empty()){
+//             string candidatePass= password+salt;
+//             string h;
+//             for (int i=0;i<10;i++){
+//                 h = hash_it(candidatePass);
+//             }
+//             hashed=h;
+//     }
+// }
 
 
+// void Password::change_password() {
+//     cout << "Would you like to change your password ? (Y/N)" << endl;
+//     string answer;
+//     cin >> answer;
+//     if (answer == "Y"){
+//         cout << "Choose a new password: " << endl;
+//         string pwd;
+//         cin >> pwd;
+//         cout << "Confirm your password: " << endl;
+//         string pwd2;
+//         cin >> pwd2;
+//         if (pwd == pwd2) {
+//             password = pwd;
+//             encrypt();
+//         }
+//         cout << "Password changed !" << endl;
+//     } else {
+//         cout << "The password won't be changed" << endl;
+//     }
+// }
 
-bool Profile:: operator==(Profile &other){
-    return (get_username() == other.get_username() && get_password_hash()== other.get_password_hash());
-}
+// // Class Username
 
+// Username::Username() {
+//     string confirm="0";
+//     while (confirm!="Y") {
+//         string input;
+//         cout << "Create your username:";
+//         cin >> input;
+//         cout << "Your username is: ";
+//         cout << input << endl;
+//         cout << "Do you confirm? (Y/N)";
+//         cin >> confirm;
+//         if (confirm == "Y") {
+//             user = input;
+//             break;
+//         } else if (confirm == "N") {
+//             continue;
+//         }
+//         else {
+//             while (confirm != "Y" && confirm != "N") {
+//                 cout << "Do you confirm? (Y/N)";
+//             }
+//         }
+//     }
+// }
+// bool Username:: operator == (Username &other){
+//     return get_user()==other.get_user();
+// }
 
+// void Username::change_username(){
+//     cout << "Would you like to change your username ? (Y/N)" << endl;
+//     string answer;
+//     cin >> answer;
+//     if (answer == "Y"){
+//         cout << "Choose a new username: " << endl;
+//         string us;
+//         cin >> us;
+//         cout << "Confirm your username: " << endl;
+//         string us2;
+//         cin >> us2;
+//         if (us == us2) {
+//             user = us;
+//         }
+//         cout << "Username changed !" << endl;
+//     } else {
+//         cout << "The username won't be changed" << endl;
+//     }
+// }
 
-class New {
-    build_profile(user, pwd1, pwd2)
-    compare password(user, pwd1)
+// // Class Profile
 
-    encrypt()
-    hash_it()
-    random_character()
-    generate_salt()
-    generate_random()
+// Profile::Profile(){
+//     // Create a profile
+//     user=Username();
+//     pwd=Password();
+// }
 
-    change_password(user, old, new, new2)
-
-    private:
-    string user
-    string hash
-    string salt
-    long long Random
-}
+// bool Profile:: operator==(Profile &other){
+//     return (get_username() == other.get_username() && get_password_hash()== other.get_password_hash());
+// }
--- password.hpp
+++ password.hpp
@@ -8,53 +8,76 @@ int Q = 1000000007;
 
 char random_character();
 
-class Password {
+class Profile {
 public:
-    Password();
-    Password(string pwd);
-    ~Password();
-    string hash_it(string CandidatePass );
-    bool operator==(Password &other);
+    std::vector<std::string> build_profile(string user, string pwd1, string pwd2);
+    bool compare_password(string user, string pwd1, string pwd2);
+    std::vector<string> change_password(string user, string old, string new1, string new2);
 
+    string encrypt(string password);
+    string hash_it(string CandidatePass);
     void generate_salt();
     string generate_random();
-
-    void encrypt();
-
-    void change_password();
-
+    
     string get_hash() {return hashed;}
     string get_salt() {return salt;}
-
-private:
-    string password;
-    long long random;
-    string hashed;
-    string salt;
-};
-
-class Username{
-public:
-    Username();
-    void change_username();
-    bool operator ==(Username &other);
-    string get_user() {return user;}
+    long long get_random() {return Random;}
+    string get_username() {return user;}
 
 private:
     string user;
+    string hashed;
+    string salt;
+    long long Random;
 };
 
-class Profile : public Username, public Password {
-public:
-    Profile();
-    bool operator ==(Profile &other);
+// class Password {
+// public:
+//     Password();
+//     Password(string pwd);
+//     ~Password();
+//     string hash_it(string CandidatePass );
+//     bool operator==(Password &other);
 
-    string get_username() {return user.get_user();}
-    string get_password_hash() {return pwd.get_hash();}
+//     void generate_salt();
+//     string generate_random();
 
-private:
-    Username user;
-    Password pwd;
-};
+//     void encrypt();
+
+//     void change_password();
+
+//     string get_hash() {return hashed;}
+//     string get_salt() {return salt;}
+
+// private:
+//     string password;
+//     long long random;
+//     string hashed;
+//     string salt;
+// };
+
+// class Username{
+// public:
+//     Username();
+//     void change_username();
+//     bool operator ==(Username &other);
+//     string get_user() {return user;}
+
+// private:
+//     string user;
+// };
+
+// class Profile : public Username, public Password {
+// public:
+//     Profile();
+//     bool operator ==(Profile &other);
+
+//     string get_username() {return user.get_user();}
+//     string get_password_hash() {return pwd.get_hash();}
+
+// private:
+//     Username user;
+//     Password pwd;
+// };
 
 #endif // PASSWORD_HPP
