3dfb5d303ec61fe1930532e60f523ff2c3435ef8
yassine-turki <turkiyassine2@gmail.com>
2022-12-05 14:12:02 +0100

    Test function

--- main.cpp
+++ main.cpp
@@ -5,6 +5,15 @@
 using namespace std;
 
 int main() {
-    Profile P=Profile();
+    string user;
+    user=cin>>"Enter username"<<endl;
+    string pwd;
+    pwd=cin>>"Enter pwd"<<endl;
+    Profile p=build_profile(user,pwd);
+    cout<<"Username:"<<p.get_username()<<endl;
+    cout<<"Salt is:"<<p.get_salt();endl;
+    cout<<"Random string used for hash"<<p.get_random()<<endl;
+    cout<<"Hashed pwd"<<p.get_hash()<<endl;
     return 0;
+
 }
