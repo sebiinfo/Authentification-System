9a028c4e4b2c4d3b5dcedc0a6005ab85f8340eed
Francisco Moreira <moreiramachadoneto@gmail.com>
2022-12-12 14:13:34 +0100

    Create Interfaces Folder

--- sources/Classification/classification.hpp
+++ sources/Classification/classification.hpp
@@ -5,28 +5,28 @@
 #ifndef AUTHENTICATION_SYSTEM_CLASSIFICATION_HPP
 #define AUTHENTICATION_SYSTEM_CLASSIFICATION_HPP
 
-#endif //AUTHENTICATION_SYSTEM_CLASSIFICATION_HPP
+#endif // AUTHENTICATION_SYSTEM_CLASSIFICATION_HPP
 
+#include "../Interfaces/Facedata.hpp"
 #include <iostream>
 #include <opencv2/core/mat.hpp>
-#include "vector_id.hpp"
 
-class Classification{
-public:
-    std::vector<vector_id> data;
+class Classification {
+  public:
+    std::vector<Facedata> data;
     int dimension_data, length_vector;
 
-    Classification(std::vector<vector_id> data);
+    Classification(std::vector<Facedata> data);
     ~Classification();
-
-
 };
 
-class KNN : public Classification{
-public:
+class KNN : public Classification {
+  public:
     int k;
-    KNN(std::vector<vector_id> data);
-    double compute_distance (vector_id query, int i); // computes Euclidian distance between a query and ith vector in data
-    static bool compare (vector_id v1, vector_id v2);
-    int classify(vector_id query);
+    KNN(std::vector<Facedata> data);
+    double compute_distance(Facedata query,
+                            int i); // computes Euclidian distance between a
+                                    // query and ith vector in data
+    static bool compare(Facedata v1, Facedata v2);
+    int classify(Facedata query);
 };
