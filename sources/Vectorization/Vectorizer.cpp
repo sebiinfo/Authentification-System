8168293f275ad1e690560b2bbcf43f875555f56b
DoBachKhoa <100733415+DoBachKhoa@users.noreply.github.com>
2022-12-15 10:32:45 +0100

    modified vectorizer file

--- sources/Vectorization/Vectorizer.cpp
+++ sources/Vectorization/Vectorizer.cpp
@@ -9,8 +9,8 @@ Vectorizer::Vectorizer(int num_people, int num_feature) {
 
 Vectorizer::~Vectorizer() {}
 
-virtual std::array<float> Vectorizer::vectorize(cv::Mat image, std::array<cv::Rect> faces) {assert(false, "Not implemented");}
+virtual std::array<float> Vectorizer::vectorize(cv::Mat image, std::vector<cv::Rect> faces) {assert(false, "Not implemented");}
 virtual std::array<float> Vectorizer::vectorize(cv::Mat image) {assert(false, "Not implemented");}
-virtual void Vectorizer::vectorize_update(cv::Mat image, std::array<cv::Rect> faces, std::vector<std::vector<float>> numerical_reps) {assert(false, "Not implemented");}
+virtual void Vectorizer::vectorize_update(cv::Mat image, std::vector<cv::Rect> faces, std::vector<std::vector<float>> numerical_reps) {assert(false, "Not implemented");}
 virtual void Vectorizer::vectorize_update(cv::Mat image, std::vector<std::vector<float>> numerical_reps) {assert(false, "Not implemented");}
 
--- sources/Vectorization/Vectorizer.hpp
+++ sources/Vectorization/Vectorizer.hpp
@@ -13,9 +13,9 @@ public:
     Vectorizer(int num_people, int num_feature);
     ~Vectorizer();
 
-    virtual std::array<float> vectorize(cv::Mat image, std::array<cv::Rect> faces);
+    virtual std::array<float> vectorize(cv::Mat image, std::vector<cv::Rect> faces);
     virtual std::array<float> vectorize(cv::Mat image);
-    virtual void vectorize_update(cv::Mat image, std::array<cv::Rect> faces, std::vector<std::vector<float>> numerical_reps);
+    virtual void vectorize_update(cv::Mat image, std::vector<cv::Rect> faces, std::vector<std::vector<float>> numerical_reps);
     virtual void vectorize_update(cv::Mat image, std::vector<std::vector<float>> numerical_reps);
 
     int num_people;
