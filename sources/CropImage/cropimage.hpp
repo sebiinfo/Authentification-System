717be1104390ddc0e86be4cb9050eb8000b2bc8f
rompoggi <ropoggi323@gmail.com>
2022-12-15 11:46:28 +0100

    Fixed order of description, Removed wrongly placed file

--- sources/CropImage/cropimage.hpp
+++ sources/CropImage/cropimage.hpp
@@ -7,7 +7,6 @@
 #include <vector>
 #include <string>
 
-
 //                                "                                       #
 //   mmm    m mm   mmm   mmmm   mmm    mmmmm   mmm    mmmm   mmm          # mm   mmmm   mmmm
 //  #"  "   #"  " #" "#  #" "#    #    # # #  "   #  #" "#  #"  #         #"  #  #" "#  #" "#
@@ -16,35 +15,35 @@
 //                       #                            m  #                       #      #
 //                       "                             ""                        "      "
 
-
 using namespace cv;
 using namespace std;
 
 // Crops in place the image at given coordinates (bottom left, bottom right, top left, top right)
-void cropOne (Mat& image, int botLeft, int botRight, int topLeft, int topRight);
+void cropOne(Mat &image, int botLeft, int botRight, int topLeft, int topRight);
 
 // Get one frame from the webcam
 Mat getFrame();
 
-void showFrame(Mat image);
 // Displays the frame given as input.
+void showFrame(Mat image);
 
 void runwebcam();
 // Opens the computer's webcam
 
-void draw(Mat& img, vector<Rect> faces,double scale,int margin);
-//draws the rectangles on the image given as input
+// draws the rectangles on the image given as input
+void draw(Mat &img, vector<Rect> faces, double scale, int margin);
 
-vector<Rect> detect(Mat& img, CascadeClassifier& cascade, double scale);
+// Returns a vector array of Rectangles holding the coordinates of faces recognised on the given image
+vector<Rect> detect(Mat &img, CascadeClassifier &cascade, double scale);
 
-void runCascadeFace(string path);
 // Continuously detetcs a face using the xml at the given path
+void runCascadeFace(string path);
 
-vector<Mat> cropWithRect(Mat frame, vector<Rect> faces);
 // Returns a list of Mat of the faces detected from the frame given
+vector<Mat> cropWithRect(Mat frame, vector<Rect> faces);
 
-vector<Mat> crop(Mat frame,string path);
 // Returns a list of Mat cropped around the face recogised by the harrcascade file linked by the path
+vector<Mat> crop(Mat frame, string path);
 
-void showCrop(Mat frame,string path);
 // Shows the images cropped from the photo
+void showCrop(Mat frame, string path);
