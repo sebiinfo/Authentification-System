23283cb3245468639db23620c42460cec0bc2b2c
Bogdan Budura <bogdanbudura@gmail.com>
2023-01-05 09:32:49 +0100

    Added query attribute

--- sources/Classification/knn.hpp
+++ sources/Classification/knn.hpp
@@ -17,10 +17,11 @@ public:
     int classify(const cv::Mat &query);
 
     // computes Euclidian distance between a
-    double compute_distance(cv::Mat query, cv::Mat vect);
+    double compute_distance(cv::Mat vect);
     std::vector<double>distance_to_query;
     int dim;
     int num_people;
+    cv::Mat query;
     std::vector<cv::Mat> num_reps;
     std::vector<cv::Mat> labels;
 
