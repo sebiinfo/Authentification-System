4250437b248863eaec5b65997bf296407ba25f47
Sebastian Simon <sebastian.simon@polytechnique.edu>
2023-01-05 09:29:48 +0100

    Small modification

--- sources/Classification/knn.cpp
+++ sources/Classification/knn.cpp
@@ -6,49 +6,49 @@
 
 KNN::KNN(int num_people, int dim, std::vector<cv::Mat> &num_reps,
          std::vector<int> &labels)
-    : Classifier(num_people, dim, num_reps, labels) {
-   k = int(sqrt(num_people));
+        : Classifier(num_people, dim, num_reps, labels) {
+    k = int(sqrt(num_people));
 }
 
-double KNN::compute_distance(cv::Mat query, int i) {
-   // todo check weather we need a weigthed sum
-   double sum = 0;
-   for (int cont = 0; cont < dim; ++cont) {
-      sum +=
-          pow(query.at<double>(0, cont) - num_reps[i].at<double>(0, cont), 2);
-   }
-   return sqrt(sum);
+double KNN::compute_distance(cv::Mat query, cv::Mat vect) {
+    // todo check weather we need a weigthed sum
+    double sum = 0;
+    for (int cont = 0; cont < dim; ++cont) {
+        sum +=
+                pow(query.at<double>(0, cont) - vect.at<double>(0, cont), 2);
+    }
+    return sqrt(sum);
 }
 
 bool KNN::compare(const cv::Mat &v1, const cv::Mat &v2) {
-   return (v1.distance_to_query < v2.distance_to_query);
+    return (compute_distance(query, v1) < v2.distance_to_query);
 }
 
 int KNN::classify(const cv::Mat &query) {
-   // first we store in the distance_to_query field of the struct the right
-   // distance
-   for (int i = 0; i < num_people; ++i) {
-      distance_to_query[i] = compute_distance(query, i);
-   }
-   // now we want to sort the data in terms of the distance, O(nlogn),
-   // n-dimensional trees will maybe be implemented later (O(n))
-   std::sort(data.begin(), data.end(), compare);
+    // first we store in the distance_to_query field of the struct the right
+    // distance
+    for (int i = 0; i < num_people; ++i) {
+        distance_to_query[i] = compute_distance(query, i);
+    }
+    // now we want to sort the data in terms of the distance, O(nlogn),
+    // n-dimensional trees will maybe be implemented later (O(n))
+    std::sort(data.begin(), data.end(), compare);
 
-   std::map<int, int> id_freq;
-   for (int i = 0; i < k; ++i) {
-      if (id_freq.count(
-              data[i].id)) {  // if the id has appeared before in the map
-         id_freq[data[i].id] += 1;
-      } else {
-         id_freq[data[i].id] = 1;
-      }
-   }
-   int max_freq = 0, max_id;
-   for (auto &it : id_freq) {
-      if (it.second > max_freq) {
-         max_id = it.first;
-         max_freq = it.second;
-      }
-   }
-   return max_id;
+    std::map<int, int> id_freq;
+    for (int i = 0; i < k; ++i) {
+        if (id_freq.count(
+                data[i].id)) {  // if the id has appeared before in the map
+            id_freq[data[i].id] += 1;
+        } else {
+            id_freq[data[i].id] = 1;
+        }
+    }
+    int max_freq = 0, max_id;
+    for (auto &it : id_freq) {
+        if (it.second > max_freq) {
+            max_id = it.first;
+            max_freq = it.second;
+        }
+    }
+    return max_id;
 }
--- sources/Classification/knn.hpp
+++ sources/Classification/knn.hpp
@@ -6,24 +6,24 @@
 #include <vector>
 
 class KNN : public Classifier {
-  public:
+public:
     int k;
 
     KNN(int num_people, int dim, std::vector<cv::Mat> &num_reps,
-              std::vector<int> &labels);
+        std::vector<int> &labels);
 
     static bool compare(const cv::Mat &v1, const cv::Mat &v2);
 
     int classify(const cv::Mat &query);
 
     // computes Euclidian distance between a
-    double compute_distance(cv::Mat query, int i); 
+    double compute_distance(cv::Mat query, cv::Mat vect);
     std::vector<double>distance_to_query;
     int dim;
     int num_people;
     std::vector<cv::Mat> num_reps;
     std::vector<cv::Mat> labels;
-     
+
     // query and ith vector in data
 };
 
