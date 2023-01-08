df9ea8bcffef514c4ff46bac979be0a9763f2c60
yassine-turki <turkiyassine2@gmail.com>
2023-01-08 20:34:23 +0100

    Complete testing function in main

--- main.cpp
+++ main.cpp
@@ -10,21 +10,40 @@
 
 
 int main(){
+    // Please name every picture you add with the word closed or open.
     DIR* dir;
     struct dirent* ent;
     std::string folder ="C:\\Users\\USER\\CLionProjects\\Authentification-System\\Authentification-System\\images\\Testing";
     dir = opendir(folder.c_str());
+    double trueclosed=0;
+    double trueopen=0;
+    double count=0;
         while ((ent = readdir(dir)) != nullptr) {
+            count+=1;
             std::string fileName = ent->d_name;
             if (fileName.size() >= 4 && fileName.substr(fileName.size() - 4) == ".jpg") {
                 cv::Mat frameclosed = cv::imread(folder+"//"+fileName);
                 //normalizeIntensities(frameclosed);
                 frameclosed=rescaleImage(frameclosed, 224,224);
                 bool eyeOpen0 = detectEyes(frameclosed);
+                if (fileName.find("closed") != std::string::npos) {
+                    if(eyeOpen0==0){
+                        trueclosed+=1;
+                    }
+                }
+                if (fileName.find("open") != std::string::npos) {
+                    if(eyeOpen0==1){
+                        trueopen+=1;
+                    }
+                }
                 std::cout<<"file: "<<fileName<< "   status: " << eyeOpen0<<std::endl;
             }
         }
         closedir(dir);
+        std::cout<<"Total number of test images:   "<<count<<std::endl;
+        std::cout<<"True openeyes: "<<trueopen<<std::endl;
+        std::cout<<"True closedeyes:  "<<trueclosed<<std::endl;
+        std::cout << "Total correct cases:  "<<trueopen+trueclosed<<"/"<<count<<std::endl;
     return 0;
     }
 
