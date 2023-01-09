3f44a57fc306ffb390f23b461b8327963f3feecd
SantorinYu01 <santorinyu@gmail.com>
2023-01-10 00:04:31 +0100

    Conpletion of liveness conputing optimal flow

--- sources/Liveness/liveness.cpp
+++ sources/Liveness/liveness.cpp
@@ -48,6 +48,88 @@ int main(int argc, char* argv[])
     std::vector<cv::Rect> faces;
     bool is_face_present = false;
 
+    while(true)
+    {
+        cap >> frame;
+        if(frame.empty())
+            break;
+
+        // converstion to grayscale
+        cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
+
+        // detect faces 
+        face_detector.detectMultiScale(gray, faces, 1.1, 3, 0, cv::Size(30, 30));
+
+        // verify if a face is present in the frame
+        if(!faces.empty())
+        {
+            is_face_present = true;
+
+            // set up the points for the optical flow
+            points[0].clear();
+            for(auto & face : faces)
+            {
+                points[0].emplace_back(face.x + face.width/2, face.y + face.height/2);
+            }
+
+            // compute the optical flow
+            if(prev_gray.empty())
+                gray.copyTo(prev_gray);
+            cv::calcOpticalFlowPyrLK(prev_gray, gray, points[0], points[1], cv::noArray(), cv::noArray(), cv::Size(31,31));
+            // compute the flow magnitude
+            std::vector<double> flow_magnitude;
+            for(size_t i = 0; i < points[1].size(); i++)
+            {
+                double dx = points[1][i].x - points[0][i].x;
+                double dy = points[1][i].y - points[0][i].y;
+                flow_magnitude.push_back(sqrt(dx*dx + dy*dy));
+            }
+
+            // compute the average flow magnitude
+            double avg_flow_magnitude = 0.0;
+            for(double i : flow_magnitude)
+            {
+                avg_flow_magnitude += i;
+            }
+            avg_flow_magnitude /= flow_magnitude.size();
+
+         
+            std::cout << "Average flow magnitude: " << avg_flow_magnitude << std::endl;
+
+            // check if the average flow magnitude is above threshold
+            if(avg_flow_magnitude > 5.0)
+            {
+                std::cout << "LIVE" << std::endl;
+            }
+            else
+            {
+                std::cout << "FAKE" << std::endl;
+            }
+
+            // draw flow
+            cflow = frame.clone();
+            for(size_t i = 0; i < points[1].size(); i++)
+            {
+                cv::line(cflow, points[0][i], points[1][i], cv::Scalar(0,255,0), 2);
+                cv::circle(cflow, points[1][i], 2, cv::Scalar(0,255,0), -1);
+            }
+        }
+        else
+        {
+            is_face_present = false;
+        }
+
+        
+        cv::imshow("Frame", cflow);
+        
+        video.write(cflow);
+        
+        cv::swap(prev_gray, gray);
+        
+        if(cv::waitKey(1) == 27)
+            break;
+    }
+    }
 
 
     // release the video capture and writer objects
