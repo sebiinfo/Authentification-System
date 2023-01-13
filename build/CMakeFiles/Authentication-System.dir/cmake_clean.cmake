cbbc88f0c20c8dfd458943cb72825075eeb397bd
gonzalezpaula <paula100803@gmail.com>
2023-01-14 00:51:08 +0100

    user interface update

---
+++ build/CMakeFiles/Authentication-System.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "Authentication-System.exe"
+  "Authentication-System.exe.manifest"
+  "Authentication-System.pdb"
+  "CMakeFiles/Authentication-System.dir/TestInterface/camera/main.cpp.obj"
+  "CMakeFiles/Authentication-System.dir/TestInterface/camera/main.cpp.obj.d"
+  "libAuthentication-System.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Authentication-System.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/sources/Mail/CMakeFiles/Mail.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/paula/OneDrive/Desktop/Paris/c++/github/Authentification-System/sources/Mail/mail.cpp" "sources/Mail/CMakeFiles/Mail.dir/mail.cpp.obj" "gcc" "sources/Mail/CMakeFiles/Mail.dir/mail.cpp.obj.d"
+  "C:/Users/paula/OneDrive/Desktop/Paris/c++/github/Authentification-System/sources/Mail/mail1.cpp" "sources/Mail/CMakeFiles/Mail.dir/mail1.cpp.obj" "gcc" "sources/Mail/CMakeFiles/Mail.dir/mail1.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
