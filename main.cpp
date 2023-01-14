8af3602adc89976ca6ae6aa449647be7288597fc
yassine-turki <turkiyassine2@gmail.com>
2023-01-14 15:56:26 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,54 +17,55 @@
 
 #if test == 0
 
-//int testeyedetection(){ //Tests isEyeOpen function on the folder "Testing"
-    // Please name every picture you add with the word closed or open.
-//    DIR* dir;
-//    struct dirent* ent;
-//    std::string folder ="C:\\Users\\USER\\CLionProjects\\Authentification-System\\Authentification-System\\images\\Testing";
-//    dir = opendir(folder.c_str());
-//    double trueclosed=0;
-//    double falseclosed=0;
-//    double falseopen=0;
-//    double trueopen=0;
-//    double count=0;
-//    while ((ent = readdir(dir)) != nullptr) {
-//        std::string fileName = ent->d_name;
-//        if (fileName.size() >= 4 && fileName.substr(fileName.size() - 4) == ".jpg") {
-//            count+=1;
-//            cv::Mat frameclosed = cv::imread(folder+"//"+fileName);
-//            //normalizeIntensities(frameclosed);
-//            frameclosed=rescaleImage(frameclosed, 224,224);
-//            bool eyeOpen0 = isEyeOpen(frameclosed);
-//            if (fileName.find("closed") != std::string::npos) {
-//                if(eyeOpen0==0){
-//                    trueclosed+=1;
-//                }
-//                else{
-//                    falseclosed+=1;
-//                }
-//            }
-//            if (fileName.find("open") != std::string::npos) {
-//                if(eyeOpen0==1){
-//                    trueopen+=1;
-//                }
-//                else{
-//                    falseopen+=1;
-//                }
-//            }
-//            std::cout<<"file: "<<fileName<< "   status: " << eyeOpen0<<std::endl;
-//        }
-//    }
-//    closedir(dir);
-//    std::cout<<std::endl;
-//    std::cout<<"Total number of test images:   "<<count<<std::endl;
-//    std::cout<<"True openeyes: "<<trueopen<<std::endl;
-//    std::cout<<"Non detected openeyes: "<<falseopen<<std::endl;
-//    std::cout<<"True closedeyes:  "<<trueclosed<<std::endl;
-//    std::cout<<"Non detected closedeyes: "<<falseclosed<<std::endl;
-//    std::cout << "Total correct cases:  "<<trueopen+trueclosed<<"/"<<count<<" = "<<((trueopen+trueclosed)/count)*100<<"%"<<std::endl;
-//    return 0;
-//}
+int testeyedetection(){ //Tests isEyeOpen function on the folder "Testing"
+     Please name every picture you add with the word closed or open.
+    DIR* dir;
+    struct dirent* ent;
+    std::string folder ="C:\\Users\\USER\\CLionProjects\\Authentification-System\\Authentification-System\\images\\Testing";
+    dir = opendir(folder.c_str());
+    double trueclosed=0;
+    double falseclosed=0;
+    double falseopen=0;
+    double trueopen=0;
+    double count=0;
+    while ((ent = readdir(dir)) != nullptr) {
+        std::string fileName = ent->d_name;
+        if (fileName.size() >= 4 && fileName.substr(fileName.size() - 4) == ".jpg") {
+            count+=1;
+            cv::Mat frameclosed = cv::imread(folder+"//"+fileName);
+            //normalizeIntensities(frameclosed);
+            frameclosed=rescaleImage(frameclosed, 224,224);
+            bool eyeOpen0 = isEyeOpen(frameclosed);
+            if (fileName.find("closed") != std::string::npos) {
+                if(eyeOpen0==0){
+                    trueclosed+=1;
+                }
+                else{
+                    falseclosed+=1;
+                }
+            }
+            if (fileName.find("open") != std::string::npos) {
+                if(eyeOpen0==1){
+                    trueopen+=1;
+                }
+                else{
+                    falseopen+=1;
+                }
+            }
+            std::cout<<"file: "<<fileName<< "   status: " << eyeOpen0<<std::endl;
+        }
+    }
+    closedir(dir);
+    std::cout<<std::endl;
+    std::cout<<"Total number of test images:   "<<count<<std::endl;
+    std::cout<<"True openeyes: "<<trueopen<<std::endl;
+    std::cout<<"Non detected openeyes: "<<falseopen<<std::endl;
+    std::cout<<"True closedeyes:  "<<trueclosed<<std::endl;
+    std::cout<<"Non detected closedeyes: "<<falseclosed<<std::endl;
+    std::cout << "Total correct cases:  "<<trueopen+trueclosed<<"/"<<count<<" = "<<((trueopen+trueclosed)/count)*100<<"%"<<std::endl;
+    return 0;
+}
+
 int test_angle() {
     DIR* dir;
     struct dirent* ent;
