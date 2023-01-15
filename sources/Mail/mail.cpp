d04dfadbfb5d6c7edcb02a8ade61b54d87af4c7e
Sophie-Claire Antoun <108881624+sophieantoun@users.noreply.github.com>
2023-01-15 18:58:49 +0100

    mail implementation
    soon to be efficient

--- sources/Mail/mail.cpp
+++ sources/Mail/mail.cpp
@@ -1,3 +1,20 @@
 #include "mail.hpp"
 #include <iostream>
 #include <Poco/Net/HTTPServer.h>
+#include <Poco/Net/MailMessage.h>
+#include <Poco/Net/MailRecipient.h>
+
+int main() {
+Poco::Net::MailMessage msg;
+msg.addRecipient (Poco::Net::MailRecipient(Poco::Net::MailRecipient::PRIMARY_RECIPIENT,"bob@example.com", "Bob"));
+msg.setSender ("Me <me@example.com>");
+msg.setSubject ("Subject");
+msg.setContent ("Content");
+
+Poco::Net::SMTPClientSession smtp ("mail.example.com");
+smtp.login ();
+smtp.sendMessage (msg);
+smtp.close ();
+
+return 0;
+}
