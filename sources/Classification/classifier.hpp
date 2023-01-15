de706c76926882f823a66e2a566b1fdfeb00a750
Sebastian Simon <sebastian.simon@polytechnique.edu>
2023-01-15 20:32:34 +0100

    Implementation of the is_alienated function which checks statistically weather the face is none of the people in num_reps

--- sources/Classification/classifier.hpp
+++ sources/Classification/classifier.hpp
@@ -27,6 +27,11 @@ class Classifier {
 */
 
 class Classifier {
+    /*The class classifier is the parent class which will inherit all the classification algorithms
+     * It has as attributes the number of people in the dataset, num_people, the vectors representing the faces in num_reps
+     * and their dimension dim, and labels which contains the id of each face
+     */
+
  public:
    Classifier(){}
    Classifier(int num_people, int dim);
@@ -34,14 +39,27 @@ class Classifier {
               std::vector<int> &labels);
    ~Classifier();
 
+//loads num_reps and labels in case the first classifier is used
    virtual void train(std::vector<cv::Mat> &num_reps, std::vector<int> &labels);
+
+   //the function which is overwritten by the inherited algorithm class
    virtual int classify(cv::Mat &image);
 
 
-   int num_people;
-   int dim;
-   std::vector<cv::Mat> num_reps;  // numerical representations of the faces
-   std::vector<int> labels;
+
+    //this is a function which checks weather the query is too far off from all the groups
+    // returns true iff the person if too far off
+   bool is_alienated(const cv::Mat& query);
+
+   //helper function for is_alienated which checks alienation on people with label : id
+    bool is_alienated_id (const cv::Mat& query, int id);
+
+    // the alienation constant is the percentage of admissible alineated coordinates
+    const double alienation_constant=0.3;
+    int num_people;
+    int dim;
+    std::vector<cv::Mat> num_reps;  // numerical representations of the faces
+    std::vector<int> labels;
 };
 
 
