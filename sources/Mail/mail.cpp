e45d19a6272c775e5ae1cb3cabfa8cc70d52d76a
Sophie-Claire Antoun <108881624+sophieantoun@users.noreply.github.com>
2023-01-18 16:14:59 +0100

    verification email

--- sources/Mail/mail.cpp
+++ sources/Mail/mail.cpp
@@ -1,47 +1,119 @@
 #include "mail.hpp"
 #include <iostream>
-#include <Poco/Net/HTTPServer.h>
-#include <Poco/Net/MailMessage.h>
-//#include <Poco/Net/MailRecipient.h>
-#include <Poco/Net/SMTPClientSession.h>
-#include <random>
+#include "Poco/Net/MailMessage.h"
+#include "Poco/Net/MailRecipient.h"
+#include "Poco/Net/SecureSMTPClientSession.h"
+#include "Poco/Net/StringPartSource.h"
+#include "Poco/Net/SSLManager.h"
+#include "Poco/Net/KeyConsoleHandler.h"
+#include "Poco/Net/ConsoleCertificateHandler.h"
+#include "Poco/SharedPtr.h"
+#include "Poco/Path.h"
+#include "Poco/Exception.h"
+#include <iostream>
+#include <cstring>
+#include <string>
+
+using namespace std;
+using Poco::Net::MailMessage;
+using Poco::Net::MailRecipient;
+using Poco::Net::SMTPClientSession;
+using Poco::Net::SecureSMTPClientSession;
+using Poco::Net::StringPartSource;
+using Poco::Net::SSLManager;
+using Poco::Net::Context;
+using Poco::Net::KeyConsoleHandler;
+using Poco::Net::PrivateKeyPassphraseHandler;
+using Poco::Net::InvalidCertificateHandler;
+using Poco::Net::ConsoleCertificateHandler;
+using Poco::SharedPtr;
+using Poco::Path;
+using Poco::Exception;
+
+class SSLInitializer
+{
+public:
+    SSLInitializer()
+    {
+        Poco::Net::initializeSSL();
+    }
+
+    ~SSLInitializer()
+    {
+        Poco::Net::uninitializeSSL();
+    }
+};
 
 
-void mail() {
-Poco::Net::MailMessage msg;
-msg.addRecipient (Poco::Net::MailRecipient(Poco::Net::MailRecipient::PRIMARY_RECIPIENT,"sophieclaireantoun0@gmail.com", "Sophie"));
-msg.setSender ("Me <sophieclaireantoun0@gmail.com>");
-msg.setSubject ("Test");
-msg.setContent ("Content");
-
-Poco::Net::SMTPClientSession smtp ("smtp.gmail.com", 465);
-smtp.login (    Poco::Net::SMTPClientSession::LoginMethod::AUTH_PLAIN,
-                "sophieclaireantoun0@gmail.com",
-                "tnofcyabsptdsqrc");
-//                "tnofcyabsptdsqrc");
-//smtp.sendMessage (msg);
-smtp.close ();
+int generateRandomNumber() {
+    srand(time(0));
+    int randomNumber = arc4random() % 99999 + 10000; // generates a random number between 10000 and 99999
+    return randomNumber;
 }
 
+bool verifyNumber(int random_generated, int userInput) {
+    int randomNumber = random_generated;
+    if (userInput == randomNumber) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+bool verification(int random_generated){
+    int userInput;
+    std::cout << "Enter a 5-digit number: ";
+    std::cin >> userInput;
+    if (verifyNumber(random_generated, userInput)) {
+        std::cout << "Congratulations! You entered the correct number." << std::endl;
+        return true;
+    }
+    else {
+        std::cout << "Sorry, that is not the correct number." << std::endl;
+        return false;
+    }
+}
+
+void mail(int random_generated){
+
+    cout << "start" << endl;
+    SSLInitializer sslInitializer;
+
+    Poco::Net::MailMessage msg;
+    Poco::SharedPtr< Poco::Net::InvalidCertificateHandler> pCert = new ConsoleCertificateHandler(false);
+    Context::Ptr pContext = new Context(Context::CLIENT_USE, "", "", "", Context::VERIFY_RELAXED, 9, true, "ALL:!ADH:!LOW:!EXP:!MD5:@STRENGTH");
+
+    std::cout << "SSLManager::instance()" << std::endl;
+    SSLManager::instance().initializeClient(0, pCert, pContext);
+
+    msg.addRecipient (Poco::Net::MailRecipient(Poco::Net::MailRecipient::PRIMARY_RECIPIENT,"sophieclaireantoun0@gmail.com", "Sophie"));
+    msg.setSender ("Me <sophieclaireantoun0@gmail.com>");
+    msg.setSubject ("Your password verification code");
+    int x = 5;
+    std::string messagecontent = "Hello! Here is the 5-digits code that you will need in order to reinitialize your password!\nDo not share is with anyone.\nYour code is: ";
+    messagecontent += std::to_string(random_generated);
+    msg.setContent (messagecontent);
+    cout << "stop1" << endl;
+    std::string host = "smtp.google.com";
+    int port = 465;
+//    Poco::Net::Context::Ptr ptrContext = new Poco::Net::Context(Poco::Net::Context::CLIENT_USE, "", "", "", Poco::Net::Context::VERIFY_RELAXED, 9, true, "ALL:!ADH:!LOW:!EXP:!MD5:@STRENGTH");
+    Poco::Net::SecureSMTPClientSession smtp("smtp.gmail.com", 587);
+    cout << "stop2" << endl;
+        cout << "login" << endl;
+        smtp.open();
+        smtp.login();
+        cout << "startTLS" << endl;
+        Poco::Net::initializeSSL();
+        smtp.startTLS(pContext);
+        cout << "login 2" << endl;
+        smtp.login (    Poco::Net::SecureSMTPClientSession::LoginMethod::AUTH_LOGIN,
+                        "sophieclaireantoun0@gmail.com",
+                        "tnofcyabsptdsqrc");
+        smtp.sendMessage (msg);
 
 
-//void Randomnumber::generaterandom() {
-//    std::random_device rd; //create a random device
-//    std::mt19937 gen(rd()); //create a Mersenne Twister generator
-//    std::uniform_int_distribution<> dis(10000, 99999); //create a distribution for 5-digit numbers
-//    int randomNum = dis(gen); //generate a random 5-digit number
-//    randomnumber=randomNum;
-//}
+    smtp.close ();
+    cout << "stop3" << endl;
 
-//bool Randomnumber::checkifsame(){
-//    int userInput;
-//    std::cout << "Almost there! We sent you an email with a 5-digit number. Please enter it for verification: ";
-//    std::cin >> userInput;
-//    if (userInput == randomnumber) {
-//        std::cout << "Congratulations! You have will be able to reset your password." << std::endl;
-//        return true;
-//    } else {
-//        std::cout << "Sorry, that is not the correct number, please try again." << std::endl;
-//        return false;
-//    }
-//}
+}
