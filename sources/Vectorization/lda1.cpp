67a88258d7511240eb533034608956a73dc13b4d
alexserban2002 <98835348+alexserban2002@users.noreply.github.com>
2023-01-19 18:15:59 +0100

    Transformed the functions into a class

--- sources/Vectorization/lda1.cpp
+++ sources/Vectorization/lda1.cpp
@@ -1,13 +1,7 @@
-//The work for this code was shared equalyl with my colleague Alexandru Serban (alexserban2002)
+//The work for this code was shared equaly by Cezara Petrui(cezarapetrui19) and Alexandru Serban (alexserban2002)
 
-#include <opencv2/core.hpp>
-#include <opencv2/core/mat.hpp>
-#include <opencv2/opencv.hpp>
-#include "eigenvalue_decomposition.cpp"
-#include <opencv2/core/base.hpp>
+#include "LinearDiscriminantAnalysis.h"
 
-cv::Mat eigenvectors;
-cv::Mat eigenvalues;
 
 static void sortMatrixColumnsByIndices(cv::InputArray input, cv::InputArray indices, cv::OutputArray output){
     cv::Mat input_1=input.getMat();
@@ -21,6 +15,8 @@ static void sortMatrixColumnsByIndices(cv::InputArray input, cv::InputArray indi
     }
 }
 
+
+// sorts a matrix by columns for given indices
 static cv::Mat sortMatrixColumnsByIndices(cv::InputArray input, cv::InputArray indices){
     cv::Mat output;
     sortMatrixColumnsByIndices(input,indices,output);
@@ -28,8 +24,57 @@ static cv::Mat sortMatrixColumnsByIndices(cv::InputArray input, cv::InputArray i
 }
 
 
+LinearDiscriminantAnalysis::LinearDiscriminantAnalysis(){
+    num_components=0;
+}
 
-void lda(cv::InputArray images, cv::InputArray labels, int num_components){
+
+LinearDiscriminantAnalysis::LinearDiscriminantAnalysis(int num_components){
+    this->num_components=num_components;
+}
+
+
+LinearDiscriminantAnalysis::LinearDiscriminantAnalysis(cv::InputArrayOfArrays images, cv::InputArray labels, int num_components=0){
+    this->num_components=num_components;
+    this->compute(images,labels);
+}
+
+
+LinearDiscriminantAnalysis::~LinearDiscriminantAnalysis()= default;
+
+
+//transforms a vector of matrices into a row matrix
+static cv::Mat asRowMatrix(cv::InputArrayOfArrays images, int array_type, double alpha=1, double beta=0){
+    size_t number_of_samples=images.total();
+    if (number_of_samples==0){
+        return cv::Mat();
+    }
+    size_t dim_reshaped=images.getMat(0).total();
+    cv::Mat data((int)number_of_samples,(int) dim_reshaped,array_type);
+    for (int i=0;i<(int)number_of_samples;i++){
+        cv::Mat row_i=data.row(i);
+        if (images.getMat(i).isContinuous()){
+            images.getMat(i).reshape(1,1).convertTo(row_i,array_type,alpha,beta);
+        }
+        else{
+            images.getMat(i).clone().reshape(1,1).convertTo(row_i,array_type,alpha,beta);
+        }
+    }
+    return data;
+}
+
+
+cv::Mat LinearDiscriminantAnalysis::get_eigenvects(){
+    return eigenvects;
+}
+
+
+cv::Mat LinearDiscriminantAnalysis::get_eigenvals(){
+    return eigenvals;
+}
+
+
+void LinearDiscriminantAnalysis::lda(cv::InputArray images, cv::InputArray labels){
     cv::Mat images_1=images.getMat();
     std::vector<int> labels_1=labels.getMat();
     cv::Mat data;
@@ -85,47 +130,28 @@ void lda(cv::InputArray images, cv::InputArray labels, int num_components){
     cv::Mat solve_matrix;
     gemm(scatter_within_inverse,scatter_between,1.0,cv::Mat(),0.0,solve_matrix);
     cv::EigenvalueDecomposition decomp(solve_matrix);
-    eigenvalues=decomp.eigenvalues().reshape(1,1);
-    eigenvectors=decomp.eigenvectors();
-    std::vector<int> indices_sorted = argsort(eigenvalues,false);
-    eigenvalues=sortMatrixColumnsByIndices(eigenvalues,indices_sorted);
-    eigenvectors=sortMatrixColumnsByIndices(eigenvectors,indices_sorted);
-    eigenvalues=cv::Mat(eigenvalues,cv::Range::all(),cv::Range(0,num_components));
-    eigenvectors=cv::Mat(eigenvectors,cv::Range::all(),cv::Range(0,num_components));
+    eigenvals=decomp.eigenvalues().reshape(1,1);
+    eigenvects=decomp.eigenvectors();
+    std::vector<int> indices_sorted = argsort(eigenvals,false);
+    eigenvals=sortMatrixColumnsByIndices(eigenvals,indices_sorted);
+    eigenvects=sortMatrixColumnsByIndices(eigenvects,indices_sorted);
+    eigenvals=cv::Mat(eigenvals,cv::Range::all(),cv::Range(0,num_components));
+    eigenvects=cv::Mat(eigenvects,cv::Range::all(),cv::Range(0,num_components));
 }
 
 
-static cv::Mat asRowMatrix(cv::InputArrayOfArrays images, int array_type, double alpha=1, double beta=0){
-    size_t number_of_samples=images.total();
-    if (number_of_samples==0){
-        return cv::Mat();
-    }
-    size_t dim_reshaped=images.getMat(0).total();
-    cv::Mat data((int)number_of_samples,(int) dim_reshaped,array_type);
-    for (int i=0;i<(int)number_of_samples;i++){
-        cv::Mat row_i=data.row(i);
-        if (images.getMat(i).isContinuous()){
-            images.getMat(i).reshape(1,1).convertTo(row_i,array_type,alpha,beta);
-        }
-        else{
-            images.getMat(i).clone().reshape(1,1).convertTo(row_i,array_type,alpha,beta);
-        }
-    }
-    return data;
-}
 
-
-void compute(cv::InputArray images, cv::InputArray labels,int num_components){
+void LinearDiscriminantAnalysis::compute(cv::InputArray images, cv::InputArray labels){
     if (images.kind()==cv::_InputArray::MAT){
-        lda(images.getMat(),labels,num_components);
+        lda(images.getMat(),labels);
     }
     else{
-        lda(asRowMatrix(images,CV_64FC1),labels,num_components);
+        lda(asRowMatrix(images,CV_64FC1),labels);
     }
 }
 
 
-cv::Mat subspaceProject(cv::InputArray weight, cv::InputArray mean, cv::InputArray images){
+cv::Mat LinearDiscriminantAnalysis::subspaceProject(cv::InputArray weight, cv::InputArray mean, cv::InputArray images){
     cv::Mat weight_1=weight.getMat();
     cv::Mat mean_1=mean.getMat();
     cv::Mat images_1=images.getMat();
@@ -144,14 +170,11 @@ cv::Mat subspaceProject(cv::InputArray weight, cv::InputArray mean, cv::InputArr
 }
 
 
-cv::Mat project(cv::InputArray images){
-    //bool dataAsRow=true;
-    //cv::
-    //if (_dataAsRow){ // true if each row represents a sample and each column represents a feature
-    return subspaceProject(eigenvectors,cv::Mat(),images);
-   // }
-    //else{
-    //return subspaceProject(eigenvectors, cv::Mat(), images.getMat().t());
-//}
-
+cv::Mat LinearDiscriminantAnalysis::project(cv::InputArray images){
+    if (dataAsRow){
+        return subspaceProject(eigenvects,cv::Mat(),images);
+    }
+    else{
+        return subspaceProject(eigenvects, cv::Mat(), images.getMat().t());
+    }
 }
