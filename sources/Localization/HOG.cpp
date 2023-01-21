865bbd2f3fc245d3c9d36a46dcb0fc76f0819191
alexserban2002 <98835348+alexserban2002@users.noreply.github.com>
2023-01-21 21:50:50 +0100

    All the features that we coded for HOG

--- sources/Localization/HOG.cpp
+++ sources/Localization/HOG.cpp
@@ -13,6 +13,32 @@ HOG::HOG(cv::Mat image, int number_of_bins=9, int cell_width=8, int cell_height=
 HOG::~HOG()=default;
 
 
+cv::Mat HOG::convert_to_grayscale(){
+    cv::Mat gray;
+    cvtColor(image, gray, cv::COLOR_RGB2GRAY);
+    return gray;
+}
+
+
+void HOG::compute_gradient(cv::Mat grad_x, cv::Mat grad_y) {
+    Sobel(image, grad_x, CV_32F, 1, 0);
+    Sobel(image, grad_y, CV_32F, 0, 1);
+}
+
+
+std::vector<cv::Mat> HOG::divide_into_cells(cv::Mat img){
+    std::vector<cv::Mat> cells;
+    for (int y = 0; y < img.rows; y += cell_size.height) {
+        for (int x = 0; x < img.cols; x += cell_size.width) {
+            cv::Rect cell_roi(x, y, cell_size.width, cell_size.height);
+            cv::Mat cell = img(cell_roi);
+            cells.push_back(cell);
+        }
+    }
+    return cells;
+}
+
+
 std::vector<cv::Mat> HOG::compute_histograms(std::vector<cv::Mat> cells){
     std::vector<cv::Mat> histograms;
     for (cv::Mat cell : cells) {
@@ -28,6 +54,7 @@ std::vector<cv::Mat> HOG::compute_histograms(std::vector<cv::Mat> cells){
     return histograms;
 }
 
+
 std::vector<cv::Mat> HOG::normalize_histograms(std::vector<cv::Mat> histograms){
     std::vector<cv::Mat> normalized_histograms;
     for (cv::Mat hist : histograms) {
@@ -36,4 +63,19 @@ std::vector<cv::Mat> HOG::normalize_histograms(std::vector<cv::Mat> histograms){
         normalized_histograms.push_back(normalized_hist);
     }
     return normalized_histograms;
+}
+
+
+cv::Mat HOG::hog_descriptor(){
+    cv::Mat gray = convert_to_grayscale();
+    cv::Mat grad_x, grad_y;
+    compute_gradient(grad_x, grad_y);
+    cv::Mat angle, mag;
+    cartToPolar(grad_x, grad_y, mag, angle, true);
+    std::vector<cv::Mat> cells = divide_into_cells(angle);
+    std::vector<cv::Mat> histograms = compute_histograms(cells);
+    std::vector<cv::Mat> normalized_histograms = normalize_histograms(histograms);
+    cv::Mat descriptor;
+    hconcat(normalized_histograms, descriptor);
+    return descriptor;
 }
